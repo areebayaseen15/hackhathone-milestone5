c0ef5b0b7d909d6c074d3ef28354773efb9d9978
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-09-17 13:17:03 +0500

    Update main.js

--- milestone 5/main.js
+++ milestone 5/main.js
@@ -1,88 +1,96 @@
-var resumeForm = document.getElementById('resumeForm');
-var resumeSection = document.getElementById('resume');
-var editButton = document.getElementById('editButton');
+var resumeForm = document.getElementById("resumeForm");
+var resumeSection = document.getElementById("resume");
+var editButton = document.getElementById("editButton");
 var isEditing = false;
-resumeForm.addEventListener('submit', function (e) {
+var imageUrl;
+resumeForm.addEventListener("submit", function (e) {
     e.preventDefault();
     if (!isEditing) {
         // Generate resume
         var resumeData = {
-            name: document.getElementById('name').value,
-            phone: document.getElementById('phone').value,
-            whatsapp: document.getElementById('whatsapp').value,
-            email: document.getElementById('email').value,
-            linkdin: document.getElementById('linkdin').value,
-            qualifications: document.getElementById('qualifications').value,
-            courses: document.getElementById('courses').value,
-            skills: document.getElementById('skills').value,
-            profession: document.getElementById('profession').value,
-            experience: document.getElementById('experience').value,
-            summary: document.getElementById('summary').value,
+            name: document.getElementById("name").value,
+            phone: document.getElementById("phone").value,
+            address: document.getElementById("address").value,
+            email: document.getElementById("email").value,
+            linkdin: document.getElementById("linkdin").value,
+            github: document.getElementById("github").value,
+            qualifications: document.getElementById("qualifications").value,
+            courses: document.getElementById("courses").value,
+            skills: document.getElementById("skills").value,
+            profession: document.getElementById("profession")
+                .value,
+            experience: document.getElementById("experience")
+                .value,
+            profile: document.getElementById("profile")
+                .value,
+            profileImage: imageUrl || ""
         };
         displayResume(resumeData);
     }
 });
 function displayResume(data) {
-    var resumeHTML = "\n        <h2>Personal Details</h2>\n        <p><strong>Name:</strong> <span>".concat(data.name, "</span></p>\n        <p><strong>Contact Number:</strong> <span>").concat(data.phone, "</span></p>\n        <p><strong>WhatsApp:</strong> <span>").concat(data.whatsapp, "</span></p>\n        <p><strong>Email:</strong> <span>").concat(data.email, "</span></p>\n        <p><strong>LinkedIn:</strong> <span>").concat(data.linkdin, "</span></p>\n      \n        <h2>Education</h2>\n        <p><strong>Qualification:</strong> <span>").concat(data.qualifications, "</span></p>\n        <p><strong>Courses:</strong> <span>").concat(data.courses, "</span></p>\n\n        <h2>Skills</h2>\n        <p><strong>Your Skills:</strong> <span>").concat(data.skills, "</span></p>\n\n        <h2>Profession</h2>\n        <p><strong>Profession:</strong> <span>").concat(data.profession, "</span></p>\n\n        <h2>Work Experience</h2>\n        <p><strong>Experience:</strong> <span>").concat(data.experience, "</span></p>\n\n        <h2>Summary</h2>\n        <p><strong>Summary:</strong> <span>").concat(data.summary, "</span></p>\n      \n        <div>\n            <button id=\"shareButton\">Share Resume</button>\n            <button id=\"downloadButton\">Download Resume</button>\n        </div>\n    ");
+    var resumeHTML = "\n       <div class=\"container\">\n        <!-- sidebar -->\n        <div class=\"sidebar\">\n            <div class=\"profile-image\">\n         <img src=\"".concat(data.profileImage, "\" alt=\"").concat(data.name, "\">\n                </div>\n            \n            <div class=\"profile-info\">\n                <h3>Profile</h3>\n                <ul>\n                    <p>").concat(data.profile, "</p>\n                   \n                </ul>\n              \n            </div>\n\n            <div class=\"contact-info\">\n                <h3>Contact Me</h3>\n                \n                <p><i class=\"fa fa-phone\"></i>").concat(data.phone, "</p>\n                <p><i class=\"fa fa-envelope\"></i>").concat(data.email, "</p>\n                <p><i class=\"fa fa-map-marker\"></i>").concat(data.linkdin, "</p>\n                \n            </div>\n            <div class=\"social-info\">\n                <h3>Professional Accounts</h3>\n                <p><strong>Github Account:</strong><br><a href=\"\">").concat(data.github, "</a> </p>\n                <p><strong>LinkedIn Profile:</strong><br><a href=\"\">").concat(data.linkdin, "</a> </p>\n            </div>\n        </div>\n        <!-- Main section -->\n            <div class=\"main\">\n                <div class=\"name\">\n            <h1>").concat(data.name, "</h1>\n            <p>").concat(data.profession, "</p>\n\n                </div>\n                <div class=\"education\">\n                <h2>Education</h2>\n                    <ul>\n                <li>").concat(data.qualifications, "</li>\n              \n            </ul>\n                </div>\n\n                <div class=\"Technical-courses\">\n                    <h2>Technical Courses</h2>\n                    <ul>\n                        <li>").concat(data.courses, "</li>\n                    </ul>\n                \n                </div>\n\n                <div class=\"skills\" id=\"skills\">\n                    <h2>Technical Skills</h2>\n                    <ul>\n                        <li> ").concat(data.skills, "</li>\n                       \n                    </ul>\n                </div>\n\n                <div class=\"experiece\">\n                    <h2>Work Experience</h2>\n                    <ul>\n                     <li> ").concat(data.experience, "</li></ul>\n                   \n                    \n                </div>\n\n                <div class=\"Reference\">\n                    <h2>Reference</h2>\n                    <p>Will be furnished upon request.</p>\n                </div>\n\n                <div class = \"SharableLink\">\n                <h2>Sharrable link: </h2>\n                 <a href = \"#\">https://hackhathone-milestone5-areeba15.vercel.app/</a>\n               </div>\n\n            </div>\n        \n    </div>\n\n     <button id=\"shareButton\">Share Resume</button>\n    <a href=\"https://hackatone-milestone1.vercel.app/\"></a>\n    <button id=\"downloadButton\">Download Resume</button>\n    ");
     resumeSection.innerHTML = resumeHTML;
     toggleFormElements(true);
     // Add event listeners for the new buttons
-    var shareButton = document.getElementById('shareButton');
-    var downloadButton = document.getElementById('downloadButton');
-    shareButton.addEventListener('click', function () {
+    var shareButton = document.getElementById("shareButton");
+    var downloadButton = document.getElementById("downloadButton");
+    shareButton.addEventListener("click", function () {
         shareResume(data);
     });
-    downloadButton.addEventListener('click', function () {
+    downloadButton.addEventListener("click", function () {
         downloadResume(data);
     });
 }
 function toggleFormElements(disable) {
-    var inputs = resumeForm.querySelectorAll('input, select, textarea');
+    var inputs = resumeForm.querySelectorAll("input, select, textarea");
     inputs.forEach(function (input) {
         input.disabled = disable;
     });
 }
 function saveChanges() {
     toggleFormElements(true);
-    editButton.textContent = 'Edit';
+    editButton.textContent = "Edit";
     isEditing = false;
 }
-editButton.addEventListener('click', function () {
+editButton.addEventListener("click", function () {
     isEditing = !isEditing;
     if (isEditing) {
         toggleFormElements(false);
-        editButton.textContent = 'Save';
+        editButton.textContent = "Save";
     }
     else {
-        resumeForm.dispatchEvent(new Event('submit'));
+        resumeForm.dispatchEvent(new Event("submit"));
         saveChanges();
     }
 });
 function shareResume(data) {
-    var resumeText = "\n        Personal Details\n        Name: ".concat(data.name, "\n        Contact Number: ").concat(data.phone, "\n        WhatsApp: ").concat(data.whatsapp, "\n        Email: ").concat(data.email, "\n        LinkedIn: ").concat(data.linkdin, "\n\n        Education\n        Qualification: ").concat(data.qualifications, "\n        Courses: ").concat(data.courses, "\n\n        Skills\n        Your Skills: ").concat(data.skills, "\n\n        Profession\n        Profession: ").concat(data.profession, "\n\n        Work Experience\n        Experience: ").concat(data.experience, "\n\n        Summary\n        Summary: ").concat(data.summary, "\n    ");
-    // Replace this URL with your own
-    var customURL = 'https://hackhathone-milestone5-areeba15.vercel.app/';
+    var resumeText = "\n        Personal Details\n       \n       <div class=\"container\">\n        <!-- sidebar -->\n        <div class=\"sidebar\">\n            <div class=\"profile-image\">\n         <img src=\"".concat(data.profileImage, "\" alt=\"").concat(data.name, "\">\n                </div>\n            \n            <div class=\"profile-info\">\n                <h3>Profile</h3>\n                <ul>\n                    <p>").concat(data.profile, "</p>\n                   \n                </ul>\n              \n            </div>\n\n            <div class=\"contact-info\">\n                <h3>Contact Me</h3>\n                \n                <p><i class=\"fa fa-phone\"></i>").concat(data.phone, "</p>\n                <p><i class=\"fa fa-envelope\"></i>").concat(data.email, "</p>\n                <p><i class=\"fa fa-map-marker\"></i>").concat(data.linkdin, "</p>\n                \n            </div>\n            <div class=\"social-info\">\n                <h3>Professional Accounts</h3>\n                <p><strong>Github Account:</strong><br><a href=\"\">").concat(data.github, "</a> </p>\n                <p><strong>LinkedIn Profile:</strong><br><a href=\"\">").concat(data.linkdin, "</a> </p>\n            </div>\n        </div>\n        <!-- Main section -->\n            <div class=\"main\">\n                <div class=\"name\">\n            <h1>").concat(data.name, "</h1>\n            <p>").concat(data.profession, "</p>\n\n                </div>\n                <div class=\"education\">\n                <h2>Education</h2>\n                    <ul>\n                <li>").concat(data.qualifications, "</li>\n              \n            </ul>\n                </div>\n\n                <div class=\"Technical-courses\">\n                    <h2>Technical Courses</h2>\n                    <ul>\n                        <li>").concat(data.courses, "</li>\n                    </ul>\n                \n                </div>\n\n                <div class=\"skills\" id=\"skills\">\n                    <h2>Technical Skills</h2>\n                    <ul>\n                        <li> ").concat(data.skills, "</li>\n                       \n                    </ul>\n                </div>\n\n                <div class=\"experiece\">\n                    <h2>Work Experience</h2>\n                    <ul>\n                     <li> ").concat(data.experience, "</li></ul>\n                   \n                    \n                </div>\n\n                <div class=\"Reference\">\n                    <h2>Reference</h2>\n                    <p>Will be furnished upon request.</p>\n                </div>\n\n                \n            </div>\n        \n    </div>\n     \n    ");
+    //  URL
+    var customURL = "https://hackhathone-milestone5-areeba15.vercel.app/";
     if (navigator.share) {
-        navigator.share({
-            title: 'My Resume',
+        navigator
+            .share({
+            title: "My Resume",
             text: resumeText,
-            url: customURL
-        }).catch(function (error) { return console.log('Error sharing', error); });
+            url: customURL,
+        })
+            .catch(function (error) { return console.log("Error sharing", error); });
     }
     else {
-        // Fallback to copying text to clipboard
-        navigator.clipboard.writeText(resumeText)
-            .then(function () { return alert('Resume copied to clipboard!'); })
-            .catch(function (error) { return console.log('Error copying text', error); });
+        navigator.clipboard
+            .writeText(resumeText)
+            .then(function () { return alert("Resume copied to clipboard!"); })
+            .catch(function (error) { return console.log("Error copying text", error); });
     }
 }
 function downloadResume(data) {
-    var resumeText = "\n        Personal Details\n        Name: ".concat(data.name, "\n        Contact Number: ").concat(data.phone, "\n        WhatsApp: ").concat(data.whatsapp, "\n        Email: ").concat(data.email, "\n        LinkedIn: ").concat(data.linkdin, "\n\n        Education\n        Qualification: ").concat(data.qualifications, "\n        Courses: ").concat(data.courses, "\n\n        Skills\n        Your Skills: ").concat(data.skills, "\n\n        Profession\n        Profession: ").concat(data.profession, "\n\n        Work Experience\n        Experience: ").concat(data.experience, "\n\n        Summary\n        Summary: ").concat(data.summary, "\n    ");
-    var blob = new Blob([resumeText], { type: 'text/plain' });
+    var resumeText = "\n      Personal Details\n       \n       <div class=\"container\">\n        <!-- sidebar -->\n        <div class=\"sidebar\">\n            <div class=\"profile-image\">\n         <img src=\"".concat(data.profileImage, "\" alt=\"").concat(data.name, "\">\n                </div>\n            \n            <div class=\"profile-info\">\n                <h3>Profile</h3>\n                <ul>\n                    <p>").concat(data.profile, "</p>\n                   \n                </ul>\n              \n            </div>\n\n            <div class=\"contact-info\">\n                <h3>Contact Me</h3>\n                \n                <p><i class=\"fa fa-phone\"></i>").concat(data.phone, "</p>\n                <p><i class=\"fa fa-envelope\"></i>").concat(data.email, "</p>\n                <p><i class=\"fa fa-map-marker\"></i>").concat(data.linkdin, "</p>\n                \n            </div>\n            <div class=\"social-info\">\n                <h3>Professional Accounts</h3>\n                <p><strong>Github Account:</strong><br><a href=\"\">").concat(data.github, "</a> </p>\n                <p><strong>LinkedIn Profile:</strong><br><a href=\"\">").concat(data.linkdin, "</a> </p>\n            </div>\n        </div>\n        <!-- Main section -->\n            <div class=\"main\">\n                <div class=\"name\">\n            <h1>").concat(data.name, "</h1>\n            <p>").concat(data.profession, "</p>\n\n                </div>\n                <div class=\"education\">\n                <h2>Education</h2>\n                    <ul>\n                <li>").concat(data.qualifications, "</li>\n              \n            </ul>\n                </div>\n\n                <div class=\"Technical-courses\">\n                    <h2>Technical Courses</h2>\n                    <ul>\n                        <li>").concat(data.courses, "</li>\n                    </ul>\n                \n                </div>\n\n                <div class=\"skills\" id=\"skills\">\n                    <h2>Technical Skills</h2>\n                    <ul>\n                        <li> ").concat(data.skills, "</li>\n                       \n                    </ul>\n                </div>\n\n                <div class=\"experiece\">\n                    <h2>Work Experience</h2>\n                    <ul>\n                     <li> ").concat(data.experience, "</li></ul>\n                   \n                    \n                </div>\n\n                <div class=\"Reference\">\n                    <h2>Reference</h2>\n                    <p>Will be furnished upon request.</p>\n                </div>\n            </div>\n        \n    </div>\n\n   \n    ");
+    var blob = new Blob([resumeText], { type: "pdf" });
     var url = URL.createObjectURL(blob);
-    var link = document.createElement('a');
+    var link = document.createElement("a");
     link.href = url;
-    link.download = 'resume.txt'; 
+    link.download = "resume.pdf";
     link.click();
     URL.revokeObjectURL(url);
 }
