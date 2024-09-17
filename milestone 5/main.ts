8ef144ed8f4d7adb047b9c656c7687d1c58292a6
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-09-17 13:17:54 +0500

    Update main.ts

--- milestone 5/main.ts
+++ milestone 5/main.ts
@@ -1,194 +1,391 @@
 
 // TypeScript interface to structure user data
-interface ResumeData {
-    name: string;
-    phone: string;
-    whatsapp: string;
-    email: string;
-    linkdin: string;
-    qualifications: string;
-    courses: string;
-    skills: string;
-    profession: string;
-    experience: string;
-    summary: string;
+interface UserDetails {
+  name: string;
+  phone: string;
+  address: string;
+  email: string;
+  linkdin: string;
+  qualifications: string;
+  courses: string;
+  skills: string;
+  profession: string;
+  experience: string;
+  profile: string;
+  github: string;
+  profileImage: string;
 }
+const resumeForm = document.getElementById("resumeForm") as HTMLFormElement;
+const resumeSection = document.getElementById("resume") as HTMLElement;
+const editButton = document.getElementById("editButton") as HTMLButtonElement;
 
-const resumeForm = document.getElementById('resumeForm') as HTMLFormElement;
-const resumeSection = document.getElementById('resume') as HTMLElement;
-const editButton = document.getElementById('editButton') as HTMLButtonElement;
 
 let isEditing = false;
+let imageUrl: string | null 
 
-resumeForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+resumeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (!isEditing) {
-        // Generate resume
-        const resumeData: ResumeData = {
-            name: (document.getElementById('name') as HTMLInputElement).value,
-            phone: (document.getElementById('phone') as HTMLInputElement).value,
-            whatsapp: (document.getElementById('whatsapp') as HTMLInputElement).value,
-            email: (document.getElementById('email') as HTMLInputElement).value,
-            linkdin: (document.getElementById('linkdin') as HTMLInputElement).value,
-            qualifications: (document.getElementById('qualifications') as HTMLSelectElement).value,
-            courses: (document.getElementById('courses') as HTMLInputElement).value,
-            skills: (document.getElementById('skills') as HTMLInputElement).value,
-            profession: (document.getElementById('profession') as HTMLInputElement).value,
-            experience: (document.getElementById('experience') as HTMLTextAreaElement).value,
-            summary: (document.getElementById('summary') as HTMLTextAreaElement).value,
-        };
+  if (!isEditing) {
+    // Generate resume
+    const resumeData: UserDetails = {
+      name: (document.getElementById("name") as HTMLInputElement).value,
+      phone: (document.getElementById("phone") as HTMLInputElement).value,
+      address: (document.getElementById("address") as HTMLInputElement).value,
+      email: (document.getElementById("email") as HTMLInputElement).value,
+      linkdin: (document.getElementById("linkdin") as HTMLInputElement).value,
+      github: (document.getElementById("github") as HTMLInputElement).value,
+      qualifications: (
+        document.getElementById("qualifications") as HTMLSelectElement
+      ).value,
+      courses: (document.getElementById("courses") as HTMLInputElement).value,
+      skills: (document.getElementById("skills") as HTMLInputElement).value,
+      profession: (document.getElementById("profession") as HTMLInputElement)
+        .value,
+      experience: (document.getElementById("experience") as HTMLTextAreaElement)
+        .value,
+      profile: (document.getElementById("profile") as HTMLTextAreaElement)
+        .value,
+     profileImage: imageUrl || ""
+       
+    };
 
-        displayResume(resumeData);
-    }
+    displayResume(resumeData);
+  }
 });
 
-function displayResume(data: ResumeData) {
-    const resumeHTML = `
-        <h2>Personal Details</h2>
-        <p><strong>Name:</strong> <span>${data.name}</span></p>
-        <p><strong>Contact Number:</strong> <span>${data.phone}</span></p>
-        <p><strong>WhatsApp:</strong> <span>${data.whatsapp}</span></p>
-        <p><strong>Email:</strong> <span>${data.email}</span></p>
-        <p><strong>LinkedIn:</strong> <span>${data.linkdin}</span></p>
-      
-        <h2>Education</h2>
-        <p><strong>Qualification:</strong> <span>${data.qualifications}</span></p>
-        <p><strong>Courses:</strong> <span>${data.courses}</span></p>
+function displayResume(data: UserDetails) {
+  const resumeHTML = `
+       <div class="container">
+        <!-- sidebar -->
+        <div class="sidebar">
+            <div class="profile-image">
+         <img src="${data.profileImage}" alt="${data.name}">
+                </div>
+            
+            <div class="profile-info">
+                <h3>Profile</h3>
+                <ul>
+                    <p>${data.profile}</p>
+                   
+                </ul>
+              
+            </div>
 
-        <h2>Skills</h2>
-        <p><strong>Your Skills:</strong> <span>${data.skills}</span></p>
-
-        <h2>Profession</h2>
-        <p><strong>Profession:</strong> <span>${data.profession}</span></p>
-
-        <h2>Work Experience</h2>
-        <p><strong>Experience:</strong> <span>${data.experience}</span></p>
-
-        <h2>Summary</h2>
-        <p><strong>Summary:</strong> <span>${data.summary}</span></p>
-      
-        <div>
-            <button id="shareButton">Share Resume</button>
-            <button id="downloadButton">Download Resume</button>
+            <div class="contact-info">
+                <h3>Contact Me</h3>
+                
+                <p><i class="fa fa-phone"></i>${data.phone}</p>
+                <p><i class="fa fa-envelope"></i>${data.email}</p>
+                <p><i class="fa fa-map-marker"></i>${data.linkdin}</p>
+                
+            </div>
+            <div class="social-info">
+                <h3>Professional Accounts</h3>
+                <p><strong>Github Account:</strong><br><a href="">${data.github}</a> </p>
+                <p><strong>LinkedIn Profile:</strong><br><a href="">${data.linkdin}</a> </p>
+            </div>
         </div>
+        <!-- Main section -->
+            <div class="main">
+                <div class="name">
+            <h1>${data.name}</h1>
+            <p>${data.profession}</p>
+
+                </div>
+                <div class="education">
+                <h2>Education</h2>
+                    <ul>
+                <li>${data.qualifications}</li>
+              
+            </ul>
+                </div>
+
+                <div class="Technical-courses">
+                    <h2>Technical Courses</h2>
+                    <ul>
+                        <li>${data.courses}</li>
+                    </ul>
+                
+                </div>
+
+                <div class="skills" id="skills">
+                    <h2>Technical Skills</h2>
+                    <ul>
+                        <li> ${data.skills}</li>
+                       
+                    </ul>
+                </div>
+
+                <div class="experiece">
+                    <h2>Work Experience</h2>
+                    <ul>
+                     <li> ${data.experience}</li></ul>
+                   
+                    
+                </div>
+
+                <div class="Reference">
+                    <h2>Reference</h2>
+                    <p>Will be furnished upon request.</p>
+                </div>
+
+                <div class = "SharableLink">
+                <h2>Sharrable link: </h2>
+                 <a href = "#">https://hackhathone-milestone5-areeba15.vercel.app/</a>
+               </div>
+
+            </div>
+        
+    </div>
+
+     <button id="shareButton">Share Resume</button>
+    <a href="https://hackatone-milestone1.vercel.app/"></a>
+    <button id="downloadButton">Download Resume</button>
     `;
 
-    resumeSection.innerHTML = resumeHTML;
-    toggleFormElements(true); 
+  resumeSection.innerHTML = resumeHTML;
+  toggleFormElements(true);
 
-    // Add event listeners for the new buttons
-    const shareButton = document.getElementById('shareButton') as HTMLButtonElement;
-    const downloadButton = document.getElementById('downloadButton') as HTMLButtonElement;
-    
-    shareButton.addEventListener('click', () => {
-        shareResume(data);
-    });
+  // Add event listeners for the new buttons
+  const shareButton = document.getElementById(
+    "shareButton"
+  ) as HTMLButtonElement;
+  const downloadButton = document.getElementById(
+    "downloadButton"
+  ) as HTMLButtonElement;
 
-    downloadButton.addEventListener('click', () => {
-        downloadResume(data);
-    });
+  shareButton.addEventListener("click", () => {
+    shareResume(data);
+  });
+
+  downloadButton.addEventListener("click", () => {
+    downloadResume(data);
+  });
 }
 
 function toggleFormElements(disable: boolean) {
-    const inputs = resumeForm.querySelectorAll('input, select, textarea');
-    inputs.forEach(input => {
-        (input as HTMLInputElement).disabled = disable;
-    });
+  const inputs = resumeForm.querySelectorAll("input, select, textarea");
+  inputs.forEach((input) => {
+    (input as HTMLInputElement).disabled = disable;
+  });
 }
 
 function saveChanges() {
-    toggleFormElements(true); 
-    editButton.textContent = 'Edit';
-    isEditing = false;
+  toggleFormElements(true);
+  editButton.textContent = "Edit";
+  isEditing = false;
 }
 
-editButton.addEventListener('click', () => {
-    isEditing = !isEditing;
-    
-    if (isEditing) {
-        toggleFormElements(false);
-        editButton.textContent = 'Save';
-    } else {
-        resumeForm.dispatchEvent(new Event('submit'));
-        saveChanges();
-    }
+editButton.addEventListener("click", () => {
+  isEditing = !isEditing;
+
+  if (isEditing) {
+    toggleFormElements(false);
+    editButton.textContent = "Save";
+  } else {
+    resumeForm.dispatchEvent(new Event("submit"));
+    saveChanges();
+  }
 });
 
-function shareResume(data: ResumeData) {
-    const resumeText = `
+function shareResume(data: UserDetails) {
+  const resumeText = `
         Personal Details
-        Name: ${data.name}
-        Contact Number: ${data.phone}
-        WhatsApp: ${data.whatsapp}
-        Email: ${data.email}
-        LinkedIn: ${data.linkdin}
+       
+       <div class="container">
+        <!-- sidebar -->
+        <div class="sidebar">
+            <div class="profile-image">
+         <img src="${data.profileImage}" alt="${data.name}">
+                </div>
+            
+            <div class="profile-info">
+                <h3>Profile</h3>
+                <ul>
+                    <p>${data.profile}</p>
+                   
+                </ul>
+              
+            </div>
 
-        Education
-        Qualification: ${data.qualifications}
-        Courses: ${data.courses}
+            <div class="contact-info">
+                <h3>Contact Me</h3>
+                
+                <p><i class="fa fa-phone"></i>${data.phone}</p>
+                <p><i class="fa fa-envelope"></i>${data.email}</p>
+                <p><i class="fa fa-map-marker"></i>${data.linkdin}</p>
+                
+            </div>
+            <div class="social-info">
+                <h3>Professional Accounts</h3>
+                <p><strong>Github Account:</strong><br><a href="">${data.github}</a> </p>
+                <p><strong>LinkedIn Profile:</strong><br><a href="">${data.linkdin}</a> </p>
+            </div>
+        </div>
+        <!-- Main section -->
+            <div class="main">
+                <div class="name">
+            <h1>${data.name}</h1>
+            <p>${data.profession}</p>
 
-        Skills
-        Your Skills: ${data.skills}
+                </div>
+                <div class="education">
+                <h2>Education</h2>
+                    <ul>
+                <li>${data.qualifications}</li>
+              
+            </ul>
+                </div>
 
-        Profession
-        Profession: ${data.profession}
+                <div class="Technical-courses">
+                    <h2>Technical Courses</h2>
+                    <ul>
+                        <li>${data.courses}</li>
+                    </ul>
+                
+                </div>
 
-        Work Experience
-        Experience: ${data.experience}
+                <div class="skills" id="skills">
+                    <h2>Technical Skills</h2>
+                    <ul>
+                        <li> ${data.skills}</li>
+                       
+                    </ul>
+                </div>
 
-        Summary
-        Summary: ${data.summary}
+                <div class="experiece">
+                    <h2>Work Experience</h2>
+                    <ul>
+                     <li> ${data.experience}</li></ul>
+                   
+                    
+                </div>
+
+                <div class="Reference">
+                    <h2>Reference</h2>
+                    <p>Will be furnished upon request.</p>
+                </div>
+
+                
+            </div>
+        
+    </div>
+     
     `;
 
-    //  URL 
-    const customURL = 'https://hackhathone-milestone5-areeba15.vercel.app/';
+  //  URL
+  const customURL = "https://hackhathone-milestone5-areeba15.vercel.app/";
 
-    if (navigator.share) {
-        navigator.share({
-            title: 'My Resume',
-            text: resumeText,
-            url: customURL
-        }).catch(error => console.log('Error sharing', error));
-    } else {
-        
-        navigator.clipboard.writeText(resumeText)
-            .then(() => alert('Resume copied to clipboard!'))
-            .catch(error => console.log('Error copying text', error));
-    }
+  if (navigator.share) {
+    navigator
+      .share({
+        title: "My Resume",
+        text: resumeText,
+        url: customURL,
+      })
+      .catch((error) => console.log("Error sharing", error));
+  } else {
+    navigator.clipboard
+      .writeText(resumeText)
+      .then(() => alert("Resume copied to clipboard!"))
+      .catch((error) => console.log("Error copying text", error));
+  }
 }
 
-function downloadResume(data: ResumeData) {
-    const resumeText = `
-        Personal Details
-        Name: ${data.name}
-        Contact Number: ${data.phone}
-        WhatsApp: ${data.whatsapp}
-        Email: ${data.email}
-        LinkedIn: ${data.linkdin}
+function downloadResume(data: UserDetails) {
+  const resumeText = `
+      Personal Details
+       
+       <div class="container">
+        <!-- sidebar -->
+        <div class="sidebar">
+            <div class="profile-image">
+         <img src="${data.profileImage}" alt="${data.name}">
+                </div>
+            
+            <div class="profile-info">
+                <h3>Profile</h3>
+                <ul>
+                    <p>${data.profile}</p>
+                   
+                </ul>
+              
+            </div>
 
-        Education
-        Qualification: ${data.qualifications}
-        Courses: ${data.courses}
+            <div class="contact-info">
+                <h3>Contact Me</h3>
+                
+                <p><i class="fa fa-phone"></i>${data.phone}</p>
+                <p><i class="fa fa-envelope"></i>${data.email}</p>
+                <p><i class="fa fa-map-marker"></i>${data.linkdin}</p>
+                
+            </div>
+            <div class="social-info">
+                <h3>Professional Accounts</h3>
+                <p><strong>Github Account:</strong><br><a href="">${data.github}</a> </p>
+                <p><strong>LinkedIn Profile:</strong><br><a href="">${data.linkdin}</a> </p>
+            </div>
+        </div>
+        <!-- Main section -->
+            <div class="main">
+                <div class="name">
+            <h1>${data.name}</h1>
+            <p>${data.profession}</p>
 
-        Skills
-        Your Skills: ${data.skills}
+                </div>
+                <div class="education">
+                <h2>Education</h2>
+                    <ul>
+                <li>${data.qualifications}</li>
+              
+            </ul>
+                </div>
 
-        Profession
-        Profession: ${data.profession}
+                <div class="Technical-courses">
+                    <h2>Technical Courses</h2>
+                    <ul>
+                        <li>${data.courses}</li>
+                    </ul>
+                
+                </div>
 
-        Work Experience
-        Experience: ${data.experience}
+                <div class="skills" id="skills">
+                    <h2>Technical Skills</h2>
+                    <ul>
+                        <li> ${data.skills}</li>
+                       
+                    </ul>
+                </div>
 
-        Summary
-        Summary: ${data.summary}
+                <div class="experiece">
+                    <h2>Work Experience</h2>
+                    <ul>
+                     <li> ${data.experience}</li></ul>
+                   
+                    
+                </div>
+
+                <div class="Reference">
+                    <h2>Reference</h2>
+                    <p>Will be furnished upon request.</p>
+                </div>
+            </div>
+        
+    </div>
+
+   
     `;
     
-    const blob = new Blob([resumeText], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'resume.txt';
-    link.click();
-    URL.revokeObjectURL(url);
+   
+
+   
+
+  const blob = new Blob([resumeText], { type: "pdf" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "resume.pdf";
+  link.click();
+  URL.revokeObjectURL(url);
 }
